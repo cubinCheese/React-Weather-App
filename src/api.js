9d02ca956dda6d05fecfa45e81e016bec703edf2
cubinCheese <linalbert24@gmail.com>
2023-12-13 23:17:40 -0800

    resolved issue #2, search bar result cycling between loading and no-options, displayed as multiple GET requests to geo api url.

---
+++ src/api.js
@@ -0,0 +1,13 @@
+
+// fetch method for geo API
+// copied from geoAPI
+// exported as geoApiOptions
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e1bd02de68mshc7d5027fdcb6fbdp1b4e14jsnb59f1484faae',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
---
+++ src/components/search/search.js
@@ -0,0 +1,47 @@
+// Uses Async Paginate package
+
+// Search function
+
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../api";
+
+const Search = ({ onSearchChange }) => {
+
+    const [search, setSearch] = useState(null);
+
+    const loadOptions = (inputValue) => {
+        return fetch(
+            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+            geoApiOptions
+        )
+        .then((response) => response.json())
+        .then((response) => {
+          return {
+            options: response.data.map((city) => {
+              return {
+                value: `${city.latitude} ${city.longitude}`,
+                label: `${city.name}, ${city.countryCode}`,
+              };
+            }),
+          };
+        });
+    };
+    
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    };
+
+    return (
+        <AsyncPaginate
+            placeholder="Search for city"
+            debounceTimeout={600} // timeout for search key press
+            value={search}
+            onChange={handleOnChange} // fetch weather data and pass to widgets
+            loadOptions={loadOptions}
+        />
+    );
+};
+
+export default Search;
