1a9838f32ba2176f2b9c71b6386fda68ef0895d6
cubinCheese <linalbert24@gmail.com>
2023-12-14 00:26:32 -0800

    implemented css display of various weather details, labeled under .bottom. finished within current-weather.css

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -11,6 +11,30 @@ const CurrentWeather = () => {
         </div>
         <img alt="weather" className="weather-icon" src="icons/01d.png" />
       </div>
+      <div className="bottom">
+        <p className="temperature">18°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label top">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">22°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">2 mph</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">10%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">12 hPa</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
